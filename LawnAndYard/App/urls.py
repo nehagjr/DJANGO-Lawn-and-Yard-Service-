9de694a6b20aa64af9bc2b16bc482c4e60242946
nehagjr <pneha3169@gmail.com>
2024-08-03 06:44:33 +0530

    login complete

--- LawnAndYard/App/urls.py
+++ LawnAndYard/App/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path,include
+from django.urls import path
 from .views import *
 urlpatterns = [
    path('',index, name='index'),
-   path('login',login,name='login')
-
+   path('login',login,name='login'),
+   path("dashboard/",registerdata,name='dashboard'),
+   path('dashboard',dashboard,name='maindashboard'),
 ]
--- LawnAndYard/App/views.py
+++ LawnAndYard/App/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-
+from .models import Customer
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def login(request):
     return render(request,'loginForm.html')
+
+
+def registerdata(request):
+    print(request.method)
+    print(request.POST)
+    name=request.POST.get("name")
+    email=request.POST.get("email")
+    phone=request.POST.get("phone")
+    password=request.POST.get("password")
+    re_password=request.POST.get("re_password")
+    # print(name,email,phone,password,re_password)
+
+    Customer.objects.create(Name=name,Email=email,Contect=phone,Password=password,Re_Password=re_password)
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
